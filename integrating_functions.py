969c24273f7f398368a1482164e479aa5d6937fe
Carlos Ordaz <cordaz000@citymail.cuny.edu>
2022-09-20 18:32:41 -0400

    Includes func to plot val int as func x

--- integrating_functions.py
+++ integrating_functions.py
@@ -1,4 +1,5 @@
 #make functions to integrate
+import numpy as np
 
 #trapezoidal rule integrator
 def trap_integral(a,b,N,f):
@@ -31,14 +32,34 @@ def simpson_integral(a,b,N,f):
     
     #handle odd terms
     s_odd = 0
-    for k in range(1,N,2):
+    for k in range(1,N - 1,2):
         
         s_odd = (s_odd + f(a + k*h) )
 
     #handle even terms
     s_even = 0
-    for k in range(2,N,2):
+    for k in range(2,N - 2,2):
         s_even = (s_even + f(a + k*h))
 
     return h*( (f(a)+f(b))/3 + (4/3)*s_odd + (2/3)*s_even)
 
+def integral_area_function_x(delxs, integrator_func, func):
+    """
+    Provided a list with tuples of intervals (in this case spaced
+    with del_x = 0.1, thirty of them), a function to calculate the 
+    integral via Trapezoid rule and a function to integrate, 
+    return an array as long as the # of tuples having the area of 
+    all previous trapezoids summed together, i.e, the last element in
+    the array will be the total area of the integral over interval 
+    specified.
+    """
+
+    area_holder = list()
+    
+    for tup in delxs:
+        area_holder.append(integrator_func(tup[0], tup[1], 1, func))
+    
+    area_holder = np.array(area_holder)
+
+
+    return np.array( [ 0 if i== 0 else np.sum(area_holder[:i]) for i in range(len(area_holder)) ])
